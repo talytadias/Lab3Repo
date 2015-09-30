204ec643ccfa03f00e3881a99874e1a4c2dd051f
talytadias <talytacapitan@gmail.com>
2015-09-30 15:32:26 +0000

    Project Euler - Problem 25

--- Lab3/Fib.py
+++ Lab3/Fib.py
@@ -1,1 +1,12 @@
+a = 1 #f1
+b = 1 #f2
+r = 2 #f3
+while (len(str(b))) < 1000: # while size of b is less than 1000
+    a, b = b, a + b # a equals to b and b equals to a + current value of b
+    r = r + 1 # add 1 to current value of r
 
+print("The first term in the Fibonacci sequence to contain 1000 digits is:")
+print(r)
+
+#print("And the 1000 digit number is:")
+#print(b)
